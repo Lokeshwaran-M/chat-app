5ef8183f40b29dee76cee776c1d882b15e015fe5
Lokeshwaran-M <80915494+Lokeshwaran-M@users.noreply.github.com>
2022-06-08 21:23:49 +0530

    starting

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # chat-app
+
++ elements
+ - 1 User login
+ - 2 chat room
+ - 3 group chat - invite link with admin access and friends in group identification
+ - 4 sharing medias - sharing pic and video call 
+ - 5 finding Location (exception)
+
---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+/* chat application 
+
+      elements
+ 1 User login
+ 2 chat room
+ 3 group chat - invite link with admin access and friends in group identification
+ 4 sharing medias - sharing pic and video call 
+ 5 finding Location (exception)
+*/
+
+// initial login
+function login() {
+    /**
+     * asign a user ID
+     */
+    console.log("login")
+}
+
+function groupChat() {
+    /**
+     * generating a code for a chat room 
+     * giving admin access
+     */
+    console.log("generating a code for a chat room ")
+    console.log("adding peoples")
+}
+// chat room 
+function chatRoom() {
+    /**
+     * view the partispents
+     * sending message
+     * recived messages
+     * 
+    */
+
+
+}
+
+function chatlog() {
+    /**
+     * shows the friend userui that u added
+     * show the chat room of all group chats
+     */
+}
+
